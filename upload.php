58be77fa7275b5cfccae37019ff7df2201ef4314
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2015-03-06 16:27:32 +0200

    Logging bug fixed

--- upload.php
+++ upload.php
@@ -4,11 +4,12 @@ $tmp = $_FILES['data']['tmp_name'];
 $fname = 'uploads' . DIRECTORY_SEPARATOR . uniqid() . '.wav';
 
 $date = date_create();
-$date_formated = date_format('Y-m-d H:i:s');
+$date_formatted = date_format($date, 'Y-m-d H:i:s');
+
 if(move_uploaded_file($tmp, $fname)) {
-	"File uploaded to " . $fname;
+	$message = "File uploaded to " . $fname;
 } else {
-	"File upload failed";
+	$message = "File upload failed";
 }
 
-error_log("\n" . $data_formated . ' ' . $message, 3, 'server.log');
+error_log("\n" . $date_formatted . ' ' . $message, 3, 'server.log');
